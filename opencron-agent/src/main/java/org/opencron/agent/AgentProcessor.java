6b5995df0bc1d5860a574f32cd3fadec2ea9086f
benjobs <benjobs@qq.com>
2017-11-15 20:33:37 +0800

    dev...

--- opencron-agent/src/main/java/org/opencron/agent/AgentProcessor.java
+++ opencron-agent/src/main/java/org/opencron/agent/AgentProcessor.java
@@ -58,7 +58,7 @@ public class AgentProcessor implements ServerHandler,AgentJob {
     private String password;
 
     public AgentProcessor() {
-        this.password = SystemPropertyUtils.get(Constants.PARAM_OPENCRON_PASSWORD_KEY,Constants.PARAM_DEF_PASSWORD_KEY);
+        this.password = DigestUtils.md5Hex(SystemPropertyUtils.get(Constants.PARAM_OPENCRON_PASSWORD_KEY,Constants.PARAM_DEF_PASSWORD_KEY));
     }
 
     @Override
