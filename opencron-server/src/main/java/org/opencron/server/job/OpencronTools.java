aad135caa0354cd4b18f7adcf3f0789c63ba7692
benjobs <benjobs@qq.com>
2017-11-17 20:19:52 +0800

    dev...

--- opencron-server/src/main/java/org/opencron/server/job/OpencronTools.java
+++ opencron-server/src/main/java/org/opencron/server/job/OpencronTools.java
@@ -192,7 +192,7 @@ public final class OpencronTools {
 
         private static String getKeyPath() {
             if (KEY_PATH == null) {
-                KEY_PATH = System.getProperties().getProperty("user.home") + File.separator + ".opencron";
+                KEY_PATH = SystemPropertyUtils.get("user.home") + File.separator + ".opencron";
                 // 从config.properties配置都读取用户手动设置的keypath的位置,配置文件里默认没有,不建议用户指定
                 // 如果指定了位置可能会导致之前所有已可ssh登录的机器无法登陆,需要再次输入用户名密码
                 String path = PropertyPlaceholder.get(Constants.PARAM_OPENCRON_KEYPATH_KEY);
